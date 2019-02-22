58fbc33e36f9724da488f022f36bcd63d3a6d46b
Marcelo Martins Oliveira <marcelo.martins@sysmap.com.br>
2019-02-22 15:14:06 -0300

    Remove GetToken

---
+++ Sysmap.Sanity.VivoApi/Models/TokenApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sysmap.Sanity.VivoApi.Models
+{
+    public class TokenApi
+    {
+        public string Token { get; set; }
+        public string Usuario { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
