cca81900edbd8536dc52837b3f949545cf523bc2
Marcelo Martins Oliveira <marcelo.martins@sysmap.com.br>
2019-03-25 11:41:02 -0300

    Add Natura
    Adicionando metodos de get lista de cenarios e post para atualizar canario x na release da natura

---
+++ Sysmap.Sanity.VivoApi/DAOs/NaturaDAO.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Sysmap.Sanity.VivoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sysmap.Sanity.VivoApi.DAOs
+{
+    public class NaturaDAO
+    {
+        private IConfiguration _configuracoes;
+        private readonly ILogger _logger;
+
+        public NaturaDAO(IConfiguration config, ILogger<NaturaDAO> logger)
+        {
+            _logger = logger;
+            _configuracoes = config;
+        }
+
+        internal List<NaturaCenarios> ListaCenarios(string cod_release, string executor)
+        {
+            List<NaturaCenarios> natura = new List<NaturaCenarios>();
+
+            try
+            {
+                string ConnectionString = _configuracoes.GetConnectionString("Sanity");
+
+                string query = @"SELECT * FROM Natura_cenariosAPI
+                                 WHERE cod_release = @cod_release AND executor = @executor;";
+
+                using (var mysqlCon = new MySqlConnection(ConnectionString))
+                {
+                    var result = mysqlCon.Query<NaturaCenarios>(query, new { cod_release, executor });
+
+                    foreach (NaturaCenarios cenario in result)
+                    {
+                        natura.Add(cenario);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Erro: {0}", ex);
+            }
+
+            return natura;
+        }
+
+        internal void UpdateCenario(string cod_release,int nCenario, int execStatus, string observacao)
+        {
+            try
+            {
+                string date = null;
+
+                if(execStatus == 4)
+                {
+                    date = DateTime.Now.ToString("yyyy/MM/dd");
+                }
+                string ConnectionString = _configuracoes.GetConnectionString("Sanity");
+
+                string query = @"UPDATE testes_natura
+                                SET
+	                                execucao_status = @execStatus,
+	                                observacao = @observacao,
+                                    data_executado = @date
+                                WHERE cod_release = @cod_release and numero_teste = @nCenario;";
+
+                using (var mysqlCon = new MySqlConnection(ConnectionString))
+                {
+                   mysqlCon.Execute(query,new { execStatus, observacao, date, cod_release, nCenario });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Erro: {0}", ex);
+            }
+        }
+    }
+}
---
+++ Sysmap.Sanity.VivoApi/Models/NaturaCenarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sysmap.Sanity.VivoApi.Models
+{
+    public class NaturaCenarios
+    {
+        public int Numero_teste { get; set; }
+        public string Sistema { get; set; }
+        public string Ambiente { get; set; }
+        public int Execucao_status { get; set; }
+        public string Cn_login { get; set; }
+        public string Cn_senha { get; set; }
+        public string Gr_login { get; set; }
+        public string Gr_senha { get; set; }
+        public string Lider_login { get; set; }
+        public string Lider_senha { get; set; }
+        public string Browser { get; set; }
+
+    }
+}
